7d464aab7c807123c90dc87753e06ee37b378eb7
gpl942 <gpl942@alumni.ku.dk>
2023-04-16 19:59:35 +0200

    Added function plot-e to py file

--- dataproject/data_proj.py
+++ dataproject/data_proj.py
@@ -1,3 +1,7 @@
 def prediction(row,rate):
     """Returns predicted values for each row variable, for the next year, given a growth rate"""
     return row[-1] * rate 
+
+def plot_e(df, variable): 
+    I = df['variables'] == variable
+    ax=df.loc[I,:].plot(x='year', y='value', style='-o', legend=False)
