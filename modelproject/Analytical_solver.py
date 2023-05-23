dcf16c1737ecb50ad759d47c86ffcde1de4202e3
gpl942 <gpl942@alumni.ku.dk>
2023-05-24 00:24:26 +0200

    - Solved level of convergence problem - Both py files commented thorougly - Added notebook comments and analyses - Added analytical solution to steady-state level of capital - Added comparison between analytical and numerical results - Added population growth graph

--- modelproject/Analytical_solver.py
+++ modelproject/Analytical_solver.py
@@ -1,59 +1,61 @@
 import sympy as sm
 from sympy import *
 
-def log_analytic(beta_val):
+# a. initialing math symbols
+n = sm.symbols('n') # population growth
+z = sm.symbols('z') # technological growth
+L0 = sm.symbols('L_0') # starting population
+Lt = sm.symbols('L_t') # young population at time t
+Ltm1 = sm.symbols('L_{t-1}') # old population at time t
+Nt = sm.symbols('N_{t}') # employed young at time t
+Kt = sm.symbols('K_t') # capital rented at time t+1
+Ktm1 = sm.symbols('K_{t-1}') # capital rented at time t
+tauw = sm.symbols('tau_w') # income tax rate
+taur = sm.symbols('tau_r') # interest tax rate
+wt = sm.symbols('w_t') # wage per unit of labor at time 
+rt = sm.symbols('r_t') # economy return on savings at time t
+rtp1 = sm.symbols('r_{t+1}') # economy return on savings between t and t+1
+rtk = sm.symbols('r_t^k') # capital rental rate
+rtb = sm.symbols('r_t^b') # bonds interest rate
+C1t = sm.symbols('c_{1t}') # consumption when young
+C2tp1 = sm.symbols('C_{2t+1}') # consumption when old
+sigma = sm.symbols('sigma') # degree of relative risk aversion
+beta = sm.symbols('beta') # future consumption discount rate
+St = sm.symbols('S_t') # aggregate savings at time t
+st = sm.symbols('s_t') # individual saving rate at time t
+Yt = sm.symbols('Y') # production at time t
+theta = sm.symbols('theta') # degree of substitutability of production factors (if CES production function)
+gamma  = sm.symbols('gamma') # optimal share of factors distribution (if CES production function)
+alpha = sm.symbols('alpha')# output elasticity to factors change (if CD production function)
+A0 = sm.symbols('A_0') # factors productivity level at start
+At = sm.symbols('A_t') # factors productivity level at time t
+Pit = sm.symbols('Pi_t') # profits at time t
+Gt = sm.symbols('G_t') # public consumption at time t
+Bt = sm.symbols('B_t') # value of bonds oustanding at time t
+Btm1 = sm.symbols('B_{t-1}') # value of bonds oustanding at time t-1
+Tt = sm.symbols('T_t') # total tax revenue at time t
+delta = sm.symbols('delta') # capital depreciation rate
+
+def log_analytic(beta_val=1.0/1.4):
     
     """Solves household maximization problem 
     for the optimal rate of savings of young individuals, 
     given logarithmic period utility function, while showing every step of the solution.
     
-    Takes no arguments."""
+    Argument:
+    - beta_val = consumption discount factor """
 
     import sympy as sm
     from IPython.display import display
 
-    # a. initialing math symbols
-    n = sm.symbols('n') # population growth
-    z = sm.symbols('z') # technological growth
-    L0 = sm.symbols('L_0') # starting population
-    Lt = sm.symbols('L_t') # young population at time t
-    Ltm1 = sm.symbols('L_{t-1}') # old population at time t
-    Nt = sm.symbols('N_{t}') # employed young at time t
-    Kt = sm.symbols('K_t') # capital rented at time t+1
-    Ktm1 = sm.symbols('K_{t-1}') # capital rented at time t
-    tauw = sm.symbols('tau_w') # income tax rate
-    taur = sm.symbols('tau_r') # interest tax rate
-    wt = sm.symbols('w_t') # wage per unit of labor at time 
-    rt = sm.symbols('r_t') # economy return on savings at time t
-    rtp1 = sm.symbols('r_{t+1}') # economy return on savings between t and t+1
-    rtk = sm.symbols('r_t^k') # capital rental rate
-    rtb = sm.symbols('r_t^b') # bonds interest rate
-    C1t = sm.symbols('c_{1t}') # consumption when young
-    C2tp1 = sm.symbols('C_{2t+1}') # consumption when old
-    sigma = sm.symbols('sigma') # degree of relative risk aversion
-    beta = sm.symbols('beta') # future consumption discount rate
-    St = sm.symbols('S_t') # aggregate savings at time t
-    st = sm.symbols('s_t') # individual saving rate at time t
-    Yt = sm.symbols('Y') # production at time t
-    theta = sm.symbols('theta') # degree of substitutability of production factors (if CES production function)
-    gamma  = sm.symbols('gamma') # optimal share of factors distribution (if CES production function)
-    alpha = sm.symbols('alpha')# output elasticity to factors change (if CD production function)
-    A0 = sm.symbols('A_0') # factors productivity level at start
-    At = sm.symbols('A_t') # factors productivity level at time t
-    Pit = sm.symbols('Pi_t') # profits at time t
-    Gt = sm.symbols('G_t') # public consumption at time t
-    Bt = sm.symbols('B_t') # value of bonds oustanding at time t
-    Btm1 = sm.symbols('B_{t-1}') # value of bonds oustanding at time t-1
-    Tt = sm.symbols('T_t') # total tax revenue at time t
-    delta = sm.symbols('delta') # capital depreciation rate
 
-    # b. define objective function
+    # b. define objective function (household utility optimization)
     LOGutility = log(C1t)+beta*log(C2tp1)
     print(f'Objective function:')
     display(LOGutility)
     print(f'\n')
 
-    # c. define budget constraints
+    # c. define budget constraints 
     savings = sm.Eq(st*(1-tauw)*wt*Nt,St)
     young_consumption = sm.Eq((1-st)*(1-tauw)*wt*Nt,C1t)
     old_consumption = sm.Eq((1+(1-taur)*rtp1)*St,C2tp1)
@@ -93,8 +95,35 @@ def log_analytic(beta_val):
     display(sol_household[0])
     print(f'\n')
 
-    # i. characterizes the solution by selecting a  
+    # i. characterizes the solution with beta value
     sol_func = sm.lambdify(args=(beta),expr=sol_household[0])
     opt_savings = sol_func(beta_val)
     print(f'Optimal value of st for beta = {beta_val}:')
     display(opt_savings)
+
+def steady_state(alpha_val = 0.5,beta_val = 1.0/1.4,n_val = 0.1):
+
+    """Solve for steady_state level of per-worker capital
+    given Log period utility and Cobb-Douglas production
+    
+    Arguments:
+
+    - alpha_val = value of output elasticity to factors change
+    - beta_val = value of the consumption discount factor
+    - n_val = population growth rate
+
+    """
+
+    import sympy as sm
+    from IPython.display import display
+
+    # a. fine steady-state capital formula
+    steady = ((1-alpha)/((1+n)*(1+1/beta)))**(1/(1-alpha))
+    display(steady)
+
+    # b. characterizes the solution with value of beta, alpha and n
+    sol_steady = sm.lambdify(args=(alpha,beta,n),expr=steady) # trasforms simpy to function
+    opt_k = sol_steady(alpha_val,beta_val,n_val) #solves function for values of beta, n and aplha
+    print(f'For alpha = {alpha_val};\nbeta = {beta_val};\nn = {n_val};\nSteady state value of capital, k_ss =')
+    display(opt_k)
+
--- modelproject/OLG_growth_model.py
+++ modelproject/OLG_growth_model.py
@@ -7,21 +7,21 @@ import matplotlib.pyplot as plt
 class OLGModelClass():
 
     def __init__(self,do_print=True):
-        """ create the model """
+        """ initialize the model """
 
         if do_print: print('initializing the model:')
 
-        self.par = SimpleNamespace()
-        self.sim = SimpleNamespace()
+        self.par = SimpleNamespace() # create simplenamespace for parameters
+        self.sim = SimpleNamespace() # create simplenamespace for simulation variables
 
         if do_print: print('calling .setup()')
-        self.setup()
+        self.setup() # calls setup function, defined below
 
         if do_print: print('calling .allocate()')
-        self.allocate()
+        self.allocate() # calls allocation function, defined below
     
     def setup(self):
-        """ baseline parameters """
+        """ setups baseline parameters """
 
         par = self.par
 
@@ -38,116 +38,126 @@ class OLGModelClass():
         # c. government
         par.tau_w = 0.0 # labor income tax
         par.tau_r = 0.0 # capital income tax
+        par.bal_budget = True
 
 
-        # d. misc
+        # d. stocks
         par.K_lag_ini = 1.0 # initial capital stock
         par.B_lag_ini = 0.0 # initial government debt
-        par.L_lag_ini = 1.0 # initial population
+
+        # e. timeframe
         par.simT = 50 # length of simulation
 
-        # e. growth parameters
+        # f. population
+        par.L_lag_ini = 1.0 # initial population
         par.n = 0.1 # population growth rate
 
-        # f. steady-state levels
+        # g. steady-state levels conditional of type of production function
         if par.production_function == 'cobb-douglas':
             par.k_ss = ((1-par.alpha)/((1+par.n)*(1+1.0/par.beta)))**(1/(1-par.alpha)) # steady-state level of capital for Log utility and Cobb Douglas production
+
 
     def allocate(self):
         """ allocate arrays for simulation """
         
-        par = self.par
+        par = self.par 
         sim = self.sim
 
-        # a. list of variables
-        household = ['C1','C2']
-        firm = ['Y','K','K_lag','k']
-        prices = ['w','rk','rb','r','rt']
-        government = ['G','T','B','balanced_budget','B_lag']
-        population = ['L','L_lag']
+        # a. create a list of variables (at period t, otherwise)
+        household = ['C1','C2'] # (C1): consumption of young, (C2): consumption of old
+        firm = ['Y','K','K_lag','k','k_lag'] # (Y): aggregate production, (K): aggregate capital, (K_lag): aggregate capital at t-1, (k): per-worker capital
+        prices = ['w','rk','rb','r','rt'] # (w): wage, (rk): capital rental rate, (rb): bonds interest rate, (r): after-depreciation return , (rt): after-tax return
+        government = ['G','T','B','balanced_budget','B_lag'] # (G): dovernment spending, (T): government income, (B): bond outstanding value, (Balanc.): boolean for respecting budget constraint, (B_lag): bond oustanding at t-1
+        population = ['L','L_lag'] # (L): population, (L_lag): population at time t
 
-        # b. allocate
-        allvarnames = household + firm + prices + government + population
-        for varname in allvarnames:
-            sim.__dict__[varname] = np.nan*np.ones(par.simT)
+        # b. creates an empty array for each variable, as an instance of the model attribute sim 
+        allvarnames = household + firm + prices + government + population # unique list of variables
+        for varname in allvarnames: # loops trough all variables in the unique list
+            sim.__dict__[varname] = np.nan*np.ones(par.simT) # creates the empty array
+
 
     def simulate(self,do_print=True):
-        """ simulate model """
+        """ simulates model """
 
-        t0 = time.time()
+        t0 = time.time() # starts time counting
 
         par = self.par
         sim = self.sim
         
-        # a. initial values
+        # a. sets initial values for stocks and population
         sim.K_lag[0] = par.K_lag_ini
+        sim.k_lag[0] = par.K_lag_ini/par.L_lag_ini
         sim.B_lag[0] = par.B_lag_ini
         sim.L_lag[0] = par.L_lag_ini
+        # print(f'\nK_lag[0] = {sim.K_lag[0]},k_lag[0] = {sim.k_lag[0]}, L_lag[0] = {sim.L_lag[0]}')
 
-        # b. iterate
+        # b. iterates over the number of periods of the simulation
         for t in range(par.simT):
-            
-            # i. simulate before s
+
+            # i. simulates variable values before s is decided
             simulate_before_s(par,sim,t)
 
-            if t == par.simT-1: continue          
+            if t == par.simT-1: continue  # stops if we are at the second-last period         
 
-            # i. find bracket to search
+            # ii. find bracket to use within which searching for the optimal saving rate
             s_min,s_max = find_s_bracket(par,sim,t)
 
-            # ii. find optimal s
-            obj = lambda s: calc_euler_error(s,par,sim,t=t)
-            result = optimize.root_scalar(obj,bracket=(s_min,s_max),method='bisect')
-            s = result.root
+            # iii. find optimal value of saving rate
+            obj = lambda s: calc_euler_error(s,par,sim,t=t) # objective function
+            result = optimize.root_scalar(obj,bracket=(s_min,s_max),method='bisect') # optimization wrt s
+            s = result.root # optimal value of s
 
-            # iii. simulate after s
+            # iv. simulates variable values after s is decided
             simulate_after_s(par,sim,t,s)
+            # print(f'\nt = {0}, K_lag[{t}] = {sim.K_lag[t]}, k_lag[{t}] = {sim.k_lag[t]}, L_lag[{t}] = {sim.L_lag[t]}')
 
-        if do_print: print(f'simulation done in {time.time()-t0:.2f} secs')
+        if do_print: print(f'\nsimulation done in {time.time()-t0:.2f} secs') # prints time elapsed from start of simulation
 
-def find_s_bracket(par,sim,t,maxiter=500,do_print=False):
+        print(f'\noptimal saving rate in period 49 = {s:3f}') # prints result in last period
+
+def find_s_bracket(par,sim,t,maxiter=10000,do_print=False):
     """ find bracket for s to search in """
 
-    # a. maximum bracket
+    # a. defines maximum bracket
     s_min = 0.0 + 1e-8 # save almost nothing
     s_max = 1.0 - 1e-8 # save almost everything
 
-    # b. saving a lot is always possible 
-    value = calc_euler_error(s_max,par,sim,t)
-    sign_max = np.sign(value)
+    # b. finds value and sign of euler error when maximum saving rate is used
+    value = calc_euler_error(s_max,par,sim,t) # stores value of euler error with s = s_max
+    sign_max = np.sign(value) # stores sign of euler error with s = s_max
     if do_print: print(f'euler-error for s = {s_max:12.8f} = {value:12.8f}')
 
     # c. find bracket      
-    lower = s_min
-    upper = s_max
+    lower = s_min # copy upper bound of bracket
+    upper = s_max # copy lower bound of bracket
 
-    it = 0
+    it = 0 # starting iteration counter
     while it < maxiter:
                 
-        # i. midpoint and value
-        s = (lower+upper)/2 # midpoint
+        # i. calculates midpoint between bracket and uses it to calculate euler error
+        s = (lower+upper)/2 
         value = calc_euler_error(s,par,sim,t)
 
         if do_print: print(f'euler-error for s = {s:12.8f} = {value:12.8f}')
 
-        # ii. check conditions
-        valid = not np.isnan(value)
-        correct_sign = np.sign(value)*sign_max < 0
+        # ii. checks conditions
+        valid = not np.isnan(value) #checks result is not a nan
+        correct_sign = np.sign(value)*sign_max < 0 # check new error value has opposite sign respect to s_max option
         
-        # iii. next step
-        if valid and correct_sign: # found!
-            s_min = s
-            s_max = upper
-            if do_print: 
+        # iii. 
+        if valid and correct_sign: 
+            s_min = s # the midpoint of the previous bracket becomes the min value of the new bracket
+            s_max = upper # upper remains the max value of bracket
+            if do_print: # prints bracket if activated
                 print(f'bracket to search in with opposite signed errors:')
                 print(f'[{s_min:12.8f}-{s_max:12.8f}]')
             return s_min,s_max
-        elif not valid: # too low s -> increase lower bound
+        elif not valid: # value is nan: too low s: need to increase lower bound
             lower = s
-        else: # too high s -> increase upper bound
+        else: # sign is not changed respect to previous s_max too high: too high s -> decrease upper bound
             upper = s
 
-        # iv. increment
+        # iv. increment iteration step
         it += 1
 
     raise Exception('cannot find bracket for s')
@@ -157,21 +167,22 @@ def calc_euler_error(s,par,sim,t):
 
     # a. simulate forward
     simulate_after_s(par,sim,t,s)
-    simulate_before_s(par,sim,t+1) # next period
+    simulate_before_s(par,sim,t+1) # simulate for the next period
 
-    # c. Euler equation
+    # c. define Euler equation sides
     LHS = sim.C1[t]**(-par.sigma)
     RHS = (1+sim.rt[t+1])*par.beta * sim.C2[t+1]**(-par.sigma)
 
-    return LHS-RHS
+    return LHS-RHS # return the difference between the RH and LH sides (Euler error)
 
 def simulate_before_s(par,sim,t):
     """ simulate forward """
 
-    if t > 0:
+    if t > 0: # for every period different from the starting point, we update lag variables.
         sim.K_lag[t] = sim.K[t-1]
         sim.B_lag[t] = sim.B[t-1]
-        sim.L_lag[t] = sim.L_lag[0]*(1.0+par.n)**t
+        sim.L_lag[t] = sim.L_lag[0]*(1+par.n)**t
+        sim.k_lag[t] = sim.K_lag[t]/sim.L_lag[t]
 
     # a. production and factor prices
     if par.production_function == 'ces':
@@ -202,37 +213,57 @@ def simulate_before_s(par,sim,t):
     sim.rt[t] = (1-par.tau_r)*sim.r[t] # after-tax return
 
     # c. consumption
-    sim.C2[t] = (1+sim.rt[t])*(sim.K_lag[t]+sim.B_lag[t])
+    sim.C2[t] = (1+sim.rt[t])*(sim.K_lag[t]+sim.B_lag[t]) #
 
     # d. government
     sim.T[t] = par.tau_r*sim.r[t]*(sim.K_lag[t]+sim.B_lag[t]) + par.tau_w*sim.w[t]*sim.L_lag[t]
-    sim.balanced_budget[:] = True 
+    
+    if par.bal_budget == True: # if government run's a balanced budget
+        sim.balanced_budget[:] = True 
 
-    if sim.balanced_budget[t]:
+    if sim.balanced_budget[t]: # imposes balance budget condition if required
         sim.G[t] = sim.T[t] - sim.r[t]*sim.B_lag[t]
 
-    sim.B[t] = (1+sim.r[t])*sim.B_lag[t] - sim.T[t] + sim.G[t]
+    sim.B[t] = (1+sim.r[t])*sim.B_lag[t] - sim.T[t] + sim.G[t] #
 
-    #e. population
+    # #e. population in period t
     sim.L[t] = sim.L_lag[t]
 
 def simulate_after_s(par,sim,t,s):
     """ simulate forward """
 
-    # a. consumption of young
+    # a. consumption of young in period t
     sim.C1[t] = (1-par.tau_w)*sim.w[t]*sim.L_lag[t]*(1.0-s)
 
     # b. end-of-period stocks
-    I = sim.Y[t] - sim.C1[t] - sim.C2[t] - sim.G[t]
-    sim.K[t] = (1-par.delta)*sim.K_lag[t] + I
-    sim.k[t] = sim.K[t]/sim.L[t]
+    I = sim.Y[t] - sim.C1[t] - sim.C2[t] - sim.G[t] # define investment of peridod t
+    sim.K[t] = (1-par.delta)*sim.K_lag[t] + I # define aggregate capital of peridod t (ready to use for period t+1)
 
 def capital_accumulation_plot(par,sim):
-        """ plots the graph for capital accumulation """
+        """ plots the graph for capital accumulation against the define steady-state level of capital 
+        
+        Attributes:
+        - par = model parameter attributes
+        - sim = model simulation atributes
+        """
 
-        fig = plt.figure(figsize=(6,6/1.5))
-        ax = fig.add_subplot(1,1,1)
-        ax.plot(sim.k,label=r'$k_{t-1}$')
-        ax.axhline(par.k_ss,ls='--',color='black',label='analytical steady state')
-        ax.legend(frameon=True)
-        fig.tight_layout()
+        fig = plt.figure(figsize=(6,6/1.5)) # decide figure size
+        ax = fig.add_subplot(1,1,1) # decide figure plots number and position
+        ax.plot(sim.k_lag,label=r'$k_{t}$') # plot the capital accumulation curve with label
+        ax.axhline(par.k_ss,ls='--',color='black',label='analytical steady state') #plots csteady-state line with label
+        ax.legend(frameon=True) # add legend box
+        fig.tight_layout() # adjust the padding between and around subplots.
+
+def population_plot(par,sim):
+        """ plots the graph for population growth 
+        
+        Attributes:
+        - par = model parameter attributes
+        - sim = model simulation atributes
+        """
+
+        fig = plt.figure(figsize=(6,6/1.5)) # decide figure size
+        ax = fig.add_subplot(1,1,1) # decide figure plots number and position
+        ax.plot(sim.L_lag,label=r'$L_{t}$') # plot the population curve with label
+        ax.legend(frameon=True) # add legend box
+        fig.tight_layout() # adjust the padding between and around subplots.
