938fb2c966bbced3b2d649d497974a6b2d8c39b1
gpl942 <gpl942@alumni.ku.dk>
2023-05-24 11:48:13 +0200

    Final clean-up: - added few comments - added parameters to be changed in the last section of the notebook - removed excess code in py file

--- modelproject/OLG_growth_model.py
+++ modelproject/OLG_growth_model.py
@@ -1,3 +1,4 @@
+# import necessary packages
 from types import SimpleNamespace
 import time
 import numpy as np
@@ -25,7 +26,7 @@ class OLGModelClass():
 
         par = self.par
 
-        # a. household
+        # a. households
         par.sigma = 1.0 # CRRA coefficient
         par.beta = 1.0/(1.0+0.4) # discount factor
 
@@ -38,8 +39,7 @@ class OLGModelClass():
         # c. government
         par.tau_w = 0.0 # labor income tax
         par.tau_r = 0.0 # capital income tax
-        par.bal_budget = True
-
+        par.bal_budget = True # imposes a balanced budget (expenses = income)
 
         # d. stocks
         par.K_lag_ini = 1.0 # initial capital stock
@@ -52,9 +52,8 @@ class OLGModelClass():
         par.L_lag_ini = 1.0 # initial population
         par.n = 0.1 # population growth rate
 
-        # g. steady-state levels conditional of type of production function
-        if par.production_function == 'cobb-douglas':
-            par.k_ss = ((1-par.alpha)/((1+par.n)*(1+1.0/par.beta)))**(1/(1-par.alpha)) # steady-state level of capital for Log utility and Cobb Douglas production
+        # g. steady-state level for LOG utility and CD production
+        par.k_ss = ((1-par.alpha)/((1+par.n)*(1+1.0/par.beta)))**(1/(1-par.alpha)) # steady-state level of capital for Log utility and Cobb Douglas production
 
 
     def allocate(self):
@@ -86,10 +85,9 @@ class OLGModelClass():
         
         # a. sets initial values for stocks and population
         sim.K_lag[0] = par.K_lag_ini
-        sim.k_lag[0] = par.K_lag_ini/par.L_lag_ini
+        sim.k_lag[0] = par.K_lag_ini/par.L_lag_ini # capital per-pworker: aggregate capital/workers
         sim.B_lag[0] = par.B_lag_ini
         sim.L_lag[0] = par.L_lag_ini
-        # print(f'\nK_lag[0] = {sim.K_lag[0]},k_lag[0] = {sim.k_lag[0]}, L_lag[0] = {sim.L_lag[0]}')
 
         # b. iterates over the number of periods of the simulation
         for t in range(par.simT):
@@ -109,7 +107,6 @@ class OLGModelClass():
 
             # iv. simulates variable values after s is decided
             simulate_after_s(par,sim,t,s)
-            # print(f'\nt = {0}, K_lag[{t}] = {sim.K_lag[t]}, k_lag[{t}] = {sim.k_lag[t]}, L_lag[{t}] = {sim.L_lag[t]}')
 
         if do_print: print(f'\nsimulation done in {time.time()-t0:.2f} secs') # prints time elapsed from start of simulation
 
@@ -181,25 +178,25 @@ def simulate_before_s(par,sim,t):
     if t > 0: # for every period different from the starting point, we update lag variables.
         sim.K_lag[t] = sim.K[t-1]
         sim.B_lag[t] = sim.B[t-1]
-        sim.L_lag[t] = sim.L_lag[0]*(1+par.n)**t
+        sim.L_lag[t] = sim.L_lag[0]*(1+par.n)**t # population grows each period
         sim.k_lag[t] = sim.K_lag[t]/sim.L_lag[t]
 
     # a. production and factor prices
     if par.production_function == 'ces':
 
-        # i. production
+        # i. production function
         sim.Y[t] = (par.alpha*sim.K_lag[t]**(-par.theta) + (1-par.alpha)*(sim.L_lag[t])**(-par.theta) )**(-1.0/par.theta)
 
-        # ii. factor prices
+        # ii. factor prices, given by profit maximisation FOC's
         sim.rk[t] = par.alpha*sim.K_lag[t]**(-par.theta-1) * sim.Y[t]**(1.0+par.theta)
         sim.w[t] = (1-par.alpha)*(sim.L_lag[t])**(-par.theta-1) * sim.Y[t]**(1.0+par.theta)
 
     elif par.production_function == 'cobb-douglas':
 
-        # i. production
+        # i. production function
         sim.Y[t] = (sim.K_lag[t]**par.alpha) * ((sim.L_lag[t])**(1-par.alpha))
 
-        # ii. factor prices
+        # ii. factor prices, given by profit maximisation FOC's
         sim.rk[t] = par.alpha * (sim.K_lag[t]**(par.alpha-1)) * ((sim.L_lag[t])**(1-par.alpha))
         sim.w[t] = (1-par.alpha) * (sim.K_lag[t]**(par.alpha)) * ((sim.L_lag[t])**(-par.alpha))
 
@@ -209,14 +206,14 @@ def simulate_before_s(par,sim,t):
 
     # b. no-arbitrage and after-tax return
     sim.r[t] = sim.rk[t]-par.delta # after-depreciation return
-    sim.rb[t] = sim.r[t] # same return on bonds
+    sim.rb[t] = sim.r[t] # the return on bonds is the same
     sim.rt[t] = (1-par.tau_r)*sim.r[t] # after-tax return
 
-    # c. consumption
-    sim.C2[t] = (1+sim.rt[t])*(sim.K_lag[t]+sim.B_lag[t]) #
+    # c. consumption of old in period t
+    sim.C2[t] = (1+sim.rt[t])*(sim.K_lag[t]+sim.B_lag[t]) 
 
-    # d. government
-    sim.T[t] = par.tau_r*sim.r[t]*(sim.K_lag[t]+sim.B_lag[t]) + par.tau_w*sim.w[t]*sim.L_lag[t]
+    # d. government activities
+    sim.T[t] = par.tau_r*sim.r[t]*(sim.K_lag[t]+sim.B_lag[t]) + par.tau_w*sim.w[t]*sim.L_lag[t] #  governement income
     
     if par.bal_budget == True: # if government run's a balanced budget
         sim.balanced_budget[:] = True 
@@ -224,10 +221,8 @@ def simulate_before_s(par,sim,t):
     if sim.balanced_budget[t]: # imposes balance budget condition if required
         sim.G[t] = sim.T[t] - sim.r[t]*sim.B_lag[t]
 
-    sim.B[t] = (1+sim.r[t])*sim.B_lag[t] - sim.T[t] + sim.G[t] #
+    sim.B[t] = (1+sim.r[t])*sim.B_lag[t] - sim.T[t] + sim.G[t] # new debt required
 
-    # #e. population in period t
-    sim.L[t] = sim.L_lag[t]
 
 def simulate_after_s(par,sim,t,s):
     """ simulate forward """
